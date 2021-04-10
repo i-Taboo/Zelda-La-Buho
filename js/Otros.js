cd3aef3e89008befaa7c58754f77052c484f8fb2
i-Taboo <82161706+i-Taboo@users.noreply.github.com>
2021-04-09 21:21:21 -0400

    Add files via upload

---
+++ js/Otros.js
@@ -0,0 +1,15 @@
+function muestramensaje(e) {
+    alert("Oye, quien te dio permiso de tocar mi nombre >:v, ¡uscale uscale!")
+}
+
+function yaquestamos(e) {
+    alert("Ya que te pasas por alli, sigueme no seas macaco :'v")
+}
+
+function esfino(e) {
+    alert("Zelda es fiiiino, hasta la caratula brilla")
+}
+
+function currucutu(e) {
+    alert("Currucutuuuuuuuuu")
+}
--- js/lightbox.js
+++ js/lightbox.js
@@ -1,13 +1,74 @@
-function sopas(e) {
-    if (document.getElementById("img-light").classList.contains("oculto")){
-        document.getElementById("img-light").classList.remove("oculto");
-        document.getElementById("img-light").classList.add("mostrarimagen");
+function imgz1(e) {
+    if (document.getElementById("imagen-galeria1").classList.contains("oculto")){
+        document.getElementById("imagen-galeria1").classList.remove("oculto");
+        document.getElementById("imagen-galeria1").classList.add("mostrarimagen");
     }
-    
-}
+}  
+    function imgz2(e) {
+        if (document.getElementById("imagen-galeria2").classList.contains("oculto")){
+            document.getElementById("imagen-galeria2").classList.remove("oculto");
+            document.getElementById("imagen-galeria2").classList.add("mostrarimagen");
+        }
+    }
+        function imgz3(e) {
+            if (document.getElementById("imagen-galeria3").classList.contains("oculto")){
+                document.getElementById("imagen-galeria3").classList.remove("oculto");
+                document.getElementById("imagen-galeria3").classList.add("mostrarimagen");
+            }
+        }
+            function imgz4(e) {
+                if (document.getElementById("imagen-galeria4").classList.contains("oculto")){
+                    document.getElementById("imagen-galeria4").classList.remove("oculto");
+                    document.getElementById("imagen-galeria4").classList.add("mostrarimagen");
+                }
+            }
+            function imgz5(e) {
+                if (document.getElementById("imagen-galeria5").classList.contains("oculto")){
+                    document.getElementById("imagen-galeria5").classList.remove("oculto");
+                    document.getElementById("imagen-galeria5").classList.add("mostrarimagen");
+                }
+            }
+            function imgz6(e) {
+                if (document.getElementById("imagen-galeria6").classList.contains("oculto")){
+                    document.getElementById("imagen-galeria6").classList.remove("oculto");
+                    document.getElementById("imagen-galeria6").classList.add("mostrarimagen");
+                }
+            }
+            function imgz7(e) {
+                if (document.getElementById("imagen-galeria7").classList.contains("oculto")){
+                    document.getElementById("imagen-galeria7").classList.remove("oculto");
+                    document.getElementById("imagen-galeria7").classList.add("mostrarimagen");
+                }
+            }
+
 function cerrarimagen(e){
-    if (document.getElementById("img-light").classList.contains("mostrarimagen")){
-        document.getElementById("img-light").classList.remove("mostrarimagen");
-        document.getElementById("img-light").classList.add("oculto");
+    if (document.getElementById("imagen-galeria1").classList.contains("mostrarimagen")){
+        document.getElementById("imagen-galeria1").classList.remove("mostrarimagen");
+        document.getElementById("imagen-galeria1").classList.add("oculto");
+    }
+    if (document.getElementById("imagen-galeria2").classList.contains("mostrarimagen")){
+        document.getElementById("imagen-galeria2").classList.remove("mostrarimagen");
+        document.getElementById("imagen-galeria2").classList.add("oculto");
+    }
+    if (document.getElementById("imagen-galeria3").classList.contains("mostrarimagen")){
+        document.getElementById("imagen-galeria3").classList.remove("mostrarimagen");
+        document.getElementById("imagen-galeria3").classList.add("oculto");
+    }
+    if (document.getElementById("imagen-galeria4").classList.contains("mostrarimagen")){
+        document.getElementById("imagen-galeria4").classList.remove("mostrarimagen");
+        document.getElementById("imagen-galeria4").classList.add("oculto");
+    }
+    if (document.getElementById("imagen-galeria5").classList.contains("mostrarimagen")){
+        document.getElementById("imagen-galeria5").classList.remove("mostrarimagen");
+        document.getElementById("imagen-galeria5").classList.add("oculto");
+    }
+    if (document.getElementById("imagen-galeria6").classList.contains("mostrarimagen")){
+        document.getElementById("imagen-galeria6").classList.remove("mostrarimagen");
+        document.getElementById("imagen-galeria6").classList.add("oculto");
+    }
+    if (document.getElementById("imagen-galeria7").classList.contains("mostrarimagen")){
+        document.getElementById("imagen-galeria7").classList.remove("mostrarimagen");
+        document.getElementById("imagen-galeria7").classList.add("oculto");
     }
 }
+
